991fb13157269b6853bc38565d02f562b82b21ce
Martin Klingenberg <martin.klingenberg@alv.no>
2022-04-19 09:52:24 +0200

    Initial commit

---
+++ custom.el
@@ -0,0 +1,23 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("d268b67e0935b9ebc427cad88ded41e875abfcc27abd409726a92e55459e0d01" "246a9596178bb806c5f41e5b571546bb6e0f4bd41a9da0df5dfbca7ec6e2250c" "7a7b1d475b42c1a0b61f3b1d1225dd249ffa1abb1b7f726aec59ac7ca3bf4dae" "1d5e33500bc9548f800f9e248b57d1b2a9ecde79cb40c0b1398dec51ee820daf" "5f19cb23200e0ac301d42b880641128833067d341d22344806cdad48e6ec62f6" "97db542a8a1731ef44b60bc97406c1eb7ed4528b0d7296997cbb53969df852d6" default))
+ '(lsp-ui-doc-delay 0.75)
+ '(lsp-ui-doc-max-height 8)
+ '(lsp-ui-doc-max-width 72)
+ '(lsp-ui-doc-show-with-mouse nil)
+ '(org-agenda-files
+   '("~/org/privat/todo.org" "/home/klingen/org/journal.org" "/home/klingen/org/projects.org" "/home/klingen/org/test.org" "/home/klingen/org/todo.org"))
+ '(package-selected-packages
+   '(vertico sqlite3 posframe org-superstar mini-frame esqlite))
+ '(vertico-posframe-mode t)
+ '(vertico-posframe-poshandler 'posframe-poshandler-frame-top-center))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
